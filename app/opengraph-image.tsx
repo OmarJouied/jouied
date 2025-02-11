40f744744df77ac4a9120caa049779b3af45455f
OmarJouied <omarjouied1421@yahoo.com>
2025-02-12 00:13:38 +0100

    open graph test working 2

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -2,6 +2,14 @@ import { ImageResponse } from 'next/og'
 import { join } from 'node:path'
 import { readFile } from 'node:fs/promises'
 
+export const alt = 'About Acme'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+export const contentType = 'image/jpg'
+
+
 export default async function Image() {
   const logoData = await readFile(join(process.cwd(), 'public/jouied.jpg'))
   const logoSrc = Uint8Array.from(logoData).buffer as unknown as string
