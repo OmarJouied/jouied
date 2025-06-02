3bd3135071ccd1382ee08dce0c618fcbb66c92c5
OmarJouied <omarjouied1421@yahoo.com>
2025-06-02 04:28:21 +0100

    fix saving form of testimonial form

--- app/api/testimonial/route.ts
+++ app/api/testimonial/route.ts
@@ -1,7 +1,11 @@
 import Testimonial from "@/app/models/Testimonial";
+import { connectDB } from "@/app/lib/db";
 
 export const POST = async (req: Request) => {
   try {
+    // Ensure database connection is established
+    await connectDB();
+
     const data = await req.json();
 
     const testimonial = new Testimonial(data);
