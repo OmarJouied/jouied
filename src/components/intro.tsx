8bd55c0e68a6b02eeb4f72b998d2f74d3690fd86
OmarJouied <omarjouied1421@yahoo.com>
2025-08-18 23:21:16 +0100

    convert ' to &apos; code

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -62,7 +62,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        Hello, I'm <span className="font-bold">Omar Jouied</span>.<br /> I'm a{" "}
+        Hello, I&apos;m <span className="font-bold">Omar Jouied</span>.<br /> I&apos;m a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">4 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
